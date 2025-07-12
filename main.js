e29333c69f9a0390fce58dbf2394780e07874c2d
DiegoFatVega <fatvegadiego@gmail.com>
2025-07-12 10:56:55 +0200

    Refactor FizzBuzz logic to include combined output for multiples of 3 and 5

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ let three_multiple = `Fizz`
 
 for (let i = 0; i < 100; i++) {
     //console.log(i);
-    if (i % 3 == 0) {
-        console.log(three_multiple);
-    } else if (i % 5 == 0) {
-        console.log(five_multiple);
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(`${i} ${three_multiple} ${five_multiple}`)
+    } else if (i % 3 == 0 && !(i % 5 == 0)) {
+        console.log(` ${i} ${three_multiple}`)
+    } else if (i % 5 == 0 && !(i % 3 == 0)) {
+        console.log(` ${i} ${five_multiple}`)
     } else console.log(i);
 }
